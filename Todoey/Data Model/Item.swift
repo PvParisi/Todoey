115a993cf988adb153fa5f60c434f1d859b7bbdd
Piervincenzo Parisi <piervi_parisi@yahoo.it>
2018-08-23 17:30:03 +0200

    code refactoring to MVC

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Piervincenzo Parisi on 22/08/18.
+//  Copyright © 2018 Piervincenzo Parisi. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
